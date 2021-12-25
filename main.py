2aa13b6ebf8683f2f9ffa9010d37938eab2154bc
Cappy Ishihara <cappy@cappuchino.xyz>
2021-12-25 20:34:16 +0700

    weird shit

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from gi.repository import Gio
 import os
 import configparser
 
-class MainWindow(Gtk.Window):
+class MainWindow(Gtk.ApplicationWindow):
     def __init__(self):
         # Import the window from glade
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("main.glade")
+        self.builder.add_from_file("main.ui")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("main_window")
         self.window.connect("destroy", Gtk.main_quit)
         # connect action1 to GtkAboutDialog
-        #self.action1.connect("activate", lambda x: self.on_action1_activate())
-        self.window.show_all()
-    def on_action1_activate(self, widget):
-        # activate the aboiut dialog
-        about = self.builder.get_object("about_dialog")
-        #self.action1 = self.builder.get_object("action1")
-        about.connect("destroy", lambda x: about.destroy())
-        about.run()
-        about.show()
+        #self.action1.connect("activate", lambda x: self.on_action1_activate()
 
+        # get the RepoList treeview
+        RepoListView = self.builder.get_object("repoListView")
+
+        # make a repolist
+        repo_list = self.builder.get_object("repo_list")
+        # repo_list is a treeview
+        # make a liststore
+        liststore = Gtk.ListStore(str, str, str, str)
+        # add the liststore to the treeview
+
+        self.window.show_all()
+    def about_dialog(self, widget, data=None):
+        about_dialog = self.builder.get_object("about_dialog")
+        about_dialog.run()
+        about_dialog.destroy()
+    def on_addRepo_clicked(self, widget, data=None):
+        print("add repo")
 # start the program
 # make the exit button work
 if __name__ == "__main__":
