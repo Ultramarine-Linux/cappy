2c878df62b916324b80bb6c26c30d7c4f74ba21f
Cappy Ishihara <cappy@cappuchino.xyz>
2021-12-30 10:39:03 +0000

    Boi/fixes

--- cappy/windows/about.py
+++ cappy/windows/about.py
@@ -9,7 +9,17 @@ class aboutWindow():
         self.builder = Gtk.Builder()
         self.builder.add_from_file("cappy/ui/about.ui")
         self.window = self.builder.get_object("about_dialog")
+        buttons = self.builder.get_object("buttons")
+        buttons.foreach(self.conf_close_button)
+        self.builder.connect_signals(self)
         self.window.show_all()
+
+
+    def conf_close_button(self, button):
+        if button.props.label == "_Close":
+            button.connect("clicked", lambda _: self.window.destroy())
+        if button.props.label == "_License":
+            button.destroy()
 
 if __name__ == "__main__":
     aboutWindow()
