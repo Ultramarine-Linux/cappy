a2576bc078b7c76034b775745ae5cb41ea0754bf
Cappy Ishihara <cappy@cappuchino.xyz>
2022-01-04 23:29:44 +0700

    pain

--- libcappy/logger.py
+++ libcappy/logger.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     format='%(asctime)s[%(name)s][%(levelname)s] %(message)s',
     # use 24 hour time format
     datefmt='[%m/%d/%Y][%H:%M:%S]',
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
 
 def info(msg):
