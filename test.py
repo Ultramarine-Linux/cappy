7db778f2e56a576e9087bf01f66c79434f053d98
Cappy Ishihara <cappy@cappuchino.xyz>
2022-01-26 13:09:21 +0700

    change tests

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-import libcappy.installer as installer
-cfg = installer.Config('example.yml')
+import sys
+import os
+import json
+args = sys.argv[1:]
+print(args)
 
-inst = installer.Installer('example.yml')
-inst.instRoot()
-inst.postInstall()
+match args[0]:
+    case 'install':
+        import libcappy.installer as installer
+        inst = installer.Installer(config='example.yml')
+        inst.instRoot()
+        inst.postInstall()
+    case 'copr':
+        import libcappy.repository as repository
+        repo = repository.Copr()
+        repos = repo.list_projects()
+        repos = json.dumps(repos, indent=4)
+        # write repos to file
+        with open('repos.json', 'w') as f:
+            f.write(repos)
